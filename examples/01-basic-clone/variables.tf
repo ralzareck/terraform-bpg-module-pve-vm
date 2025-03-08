7f0f27f54b0f57465861cffbd0a6d9e5ddfe604e
RalZareck <axelgrunig@orange.fr>
2025-03-08 17:10:37 +0100

    [Style] Fix terraform validate

--- examples/01-basic-clone/variables.tf
+++ examples/01-basic-clone/variables.tf
@@ -3,23 +3,29 @@
 # =============================================================================
 
 variable "proxmox_host" {
-  description = "Proxmox API Host. has to be in thr form IP:PORT"
+  description = "Proxmox API Host. has to be in thr form IP:PORT."
   type        = string
 }
 
 variable "proxmox_node" {
-  description = "Which node in the Proxmox cluster to start the virtual machine on during creation"
+  description = "Which node in the Proxmox cluster to start the virtual machine on during creation."
   type        = string
 }
 
 variable "proxmox_api_token_id" {
-  description = "API Token ID"
+  description = "API Token ID."
   sensitive   = true
   type        = string
 }
 
 variable "proxmox_api_token_secret" {
-  description = "API Token Secret"
+  description = "API Token Secret."
+  sensitive   = true
+  type        = string
+}
+
+variable "proxmox_ssk_private_key" {
+  description = "The SSH private key to be used when connecting via SSH."
   sensitive   = true
   type        = string
 }
